ad42c3898751d1db62a4c238cc6c426131a82abf
Bartłomiej Teodorowicz <bart.ted.cont@gmail.com>
2026-02-16 19:02:33 +0100

    Feature/auth tests
    * test(auth): add unit tests for AuthGuard functionality
    
    * test(auth): add unit tests for AccountGuard functionality
    
    * fix(auth): add missing expect to test
    
    * fix(auth): update test to throw UnauthorizedException for missing user context
    
    * fix(auth): update AccountGuard tests to throw ForbiddenException for incomplete user registration
    
    * fix(auth): add afterEach to restore mocks in AccountGuard tests

---
+++ apps/api/src/auth/guards/account.guard.spec.ts
@@ -0,0 +1,70 @@
+import {
+  ExecutionContext,
+  ForbiddenException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AccountStatus } from '../domain/AccountStatus';
+import { AccountGuard } from './account.guard';
+
+describe('AccountGuard', () => {
+  let guard: AccountGuard;
+
+  beforeEach(() => {
+    guard = new AccountGuard();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(guard).toBeDefined();
+  });
+
+  it('should throw UnauthorizedException if user context is not found', async () => {
+    const mockContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({}), // No user in request
+      }),
+    } as unknown as ExecutionContext;
+
+    await expect(guard.canActivate(mockContext)).rejects.toThrow(
+      UnauthorizedException,
+    );
+  });
+
+  it('should throw ForbiddenException if user has not completed registration process', async () => {
+    const mockContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          user: {
+            /* Incomplete user data */
+          },
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    await expect(guard.canActivate(mockContext)).rejects.toThrow(
+      ForbiddenException,
+    );
+  });
+
+  it('should return true if user is fully registered', async () => {
+    const mockUser = {
+      email: 'test@example.com',
+      id: '12345',
+    };
+    const mockContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          user: mockUser,
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    jest.spyOn(AccountStatus, 'isFullyRegistered').mockReturnValue(true);
+
+    const result = await guard.canActivate(mockContext);
+    expect(result).toBe(true);
+  });
+});
